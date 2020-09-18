df7988e13bd86eba80a6a8fda061a3349b4e270c
Milkweedd <antontab91@gmail.com>
2020-09-18 05:14:26 +0300

    commit

---
+++ src/components/SearchFlightForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AirportBoard = () => {
+  return <div className="asas">saas</div>
+}
+
+export default AirportBoard;
---
+++ src/components/SearchResultsBoard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchResults = () => {
+  return <div className="asas">saas</div>
+}
+
+export default SearchResults;
