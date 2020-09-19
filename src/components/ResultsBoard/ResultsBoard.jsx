e381306eee3c7b62d9e9cd8d5410d4a6dc425892
Milkweedd <antontab91@gmail.com>
2020-09-19 04:00:29 +0300

    commit

--- src/components/ResultsBoard/ResultsBoard.jsx
+++ src/components/ResultsBoard/ResultsBoard.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const ResultsBoard = () => {
-  return <div className="asas">два</div>
+  return (
+    <div className="results-board">
+      <div className="results-board__tabs">
+
+      </div>
+    </div>
+  )
 }
 
 export default ResultsBoard;
