4e50142a4829587447b2dff65ad6e38562233209
Milkweedd <antontab91@gmail.com>
2020-09-19 07:50:44 +0300

    commit

--- src/components/SearchFlight/SearchFlight.jsx
+++ src/components/SearchFlight/SearchFlight.jsx
@@ -9,24 +9,26 @@ const SearchFlight = () => {
         Search flight
       </h2>
       <form className="search-flight__form" action="">
-        <label
-          className="search-flight__label"
-          htmlFor="search-flight__input"
-        >
-          <SearchOutlined
-            style={{ fontSize: 44 }}
-            className="search-flight__icon"
+        <div className="search-flight__form-control">
+          <label
+            className="search-flight__label"
+            htmlFor="search-flight__input"
+          >
+            <SearchOutlined
+              style={{ fontSize: 44 }}
+              className="search-flight__icon"
+            />
+          </label>
+          <input
+            id="search-flight__input"
+            type="text"
+            className="search-flight__input"
+            placeholder="Airline, destination or flight #"
           />
-        </label>
-        <input
-          id="search-flight__input"
-          type="text"
-          className="search-flight__input"
-          placeholder="Airline, destination or flight #"
-        />
-        <button className="search-flight__btn" type="submit">
-          SEARCH
-        </button>
+          <button className="search-flight__btn" type="submit">
+            SEARCH
+          </button>
+        </div>
       </form>
     </div>
   )
