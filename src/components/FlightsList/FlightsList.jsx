3833516cc415456a0d461ef2d6efc2988a23da77
Milkweedd <antontab91@gmail.com>
2020-09-20 06:49:42 +0300

    commit

--- src/components/FlightsList/FlightsList.jsx
+++ src/components/FlightsList/FlightsList.jsx
@@ -42,6 +42,30 @@ const FlightsList = () => {
               <td>5</td>
               <td>6</td>
             </tr>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+              <td>6</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+              <td>6</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+              <td>6</td>
+            </tr>
           </tbody>
         </table>
       </div>
