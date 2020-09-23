ec5c9be87c1fd295f8afe824f7e85e5978ae6c82
Milkweedd <antontab91@gmail.com>
2020-09-23 03:05:32 +0300

    commit

--- src/components/FlightsList/FlightsList.jsx
+++ src/components/FlightsList/FlightsList.jsx
@@ -1,29 +1,35 @@
-import React from 'react';
-import { useState } from 'react'
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { FlightTakeoff, FlightLand } from '@material-ui/icons';
-import { Link, Route, Switch, useLocation } from 'react-router-dom';
+import { Link, Route, Switch, useLocation, useHistory, useParams, useRouteMatch } from 'react-router-dom';
 
-const FlightsList = () => {
+const FlightsList = ({ arrivalsList, departuresList }) => {
   const [state, setstate] = useState('departures');
 
   const toggle = () => {
     setstate(state === 'departures' ? 'arrivals' : 'departures');
   }
-  const location = useLocation();
-  console.log(location)
 
+  const needed = state === 'departures' ? departuresList : arrivalsList;
+
+  console.log(useLocation())
+  // console.log(useHistory())
+  // console.log(useParams())
+  // console.log(useRouteMatch())
+
+  console.log(arrivalsList, departuresList)
   return (
     <div className="flights-list">
 
       <div className="flights-list__tabs">
         <Link to='/departures' onClick={toggle} className={`flights-list__tab-btn flights-list__tab-btn_departures ${state === 'departures' ? "active" : ""} `}>
           <FlightTakeoff className="flights-list__icon" />
-          <span>Departures</span>
+          <span>Departures вылет </span>
         </Link>
 
         <Link to='/arrivals' onClick={toggle} className={`flights-list__tab-btn flights-list__tab-btn_arrivals ${state === 'arrivals' ? "active" : ""} `}>
           <FlightLand className="flights-list__icon" />
-          <span>Arrivals</span>
+          <span>Arrivals прилет</span>
         </Link>
       </div>
 
@@ -39,39 +45,20 @@ const FlightsList = () => {
               <th>Flight</th>
             </tr>
           </thead>
+
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-              <td>6</td>
-            </tr>
+            {needed.slice().sort((a, b) => { return a.timeLandCalc - b.timeLandCalc }).map((flight) => {
+              return (
+                <tr key={flight.ID}>
+                  <td>{flight.term}</td>
+                  <td>{}</td>
+                  <td>3</td>
+                  <td>4</td>
+                  <td>5</td>
+                  <td>6</td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </div>
@@ -79,4 +66,25 @@ const FlightsList = () => {
   )
 }
 
-export default FlightsList;
+const mapState = (state) => {
+  return {
+    arrivalsList: state.tasks.arrivals,
+    departuresList: state.tasks.departures,
+  }
+}
+
+const connector = connect(mapState, null);
+const connectedFlightsList = connector(FlightsList);
+
+export default connectedFlightsList;
+
+// timeArrExpectCalc: "2020-09-22T  04:30:00Z"     ожидаемое время когда самолет будет в терминале 
+// timeArrShedule: "2020-09-22T     04:30:00Z"     по графику когда самолет будет в терминале 
+// timeTakeofFact: "2020-09-22T     04:33:00Z"     когда прибыл в терминал фактически 
+
+// timeLandCalc: "2020-09-22T       05:55:00Z"     ожидаемое время отлета 
+// timeLandFact: "2020-09-22T       05:53:25Z"     фактическое время отлета 
+
+// timeStandCalc: "2020-09-22T      06:00:00Z"     приблизительное время прилета  
+// timeStandFact: "2020-09-22T      06:00:34Z"     фактическое время прилета  
+// timeToStand: "2020-09-22T        05:40:00Z"     время прилета 
